a28b6c755159d2ddddf6d677d4a87bcdbbc34964
Long Phan <phanvanlong14@gmail.com>
2023-05-29 02:00:30 +0700

    Second Initial

--- src/app.js
+++ src/app.js
@@ -1,3 +1,40 @@
-const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+const app = express();
 
-console.log(uuidv4());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const PORT = 3000;
+const json = {
+    "menu": {
+        "id": "file",
+        "value": "File",
+        "popup": {
+            "menuitem": [
+                { "value": "New", "onclick": "CreateNewDoc()" },
+                { "value": "Open", "onclick": "OpenDoc()" },
+                { "value": "Close", "onclick": "CloseDoc()" }
+            ]
+        }
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.get('/nextpage', (req, res) => {
+    res.send({"Data:": json.menu.popup.menuitem});
+});
+
+app.post('/', (req, res) => {
+    res.send('This is a post request');
+});
+
+app.post('/nextpage', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+
+app.listen(PORT, () => {
+    console.log('App listening on PORT ' + PORT);
+});
